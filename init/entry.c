c560fef5e42daf39731c37a742601e40de929694
stardustman <aoyunyoung@gmail.com>
2019-05-26 19:30:04 +0800

    修改输出的内容，添加输出用户名

--- init/entry.c
+++ init/entry.c
@@ -4,8 +4,15 @@
 int kern_entry()
 {
     uint8_t *input = (uint8_t *)0xB8000;
-    uint8_t color = (0 << 4) | (15 & 0x0F);
-
+    //uint8_t color = (0 << 4) | (15 & 0x0F);
+    uint8_t color = (0 << 4) | (15 & 0x2);
+    /**
+    http://wiki.0xffffff.org/posts/hurlex-4.html
+    从0xB8000这个地址开始，每2个字节表示屏幕上显示的一个字符。
+    从屏幕的第一行开始对应，一行接着一行的对应下去。
+    而这两个字节的前一个是显示字符的ASCII码，
+    后一个是控制这个字符颜色和属性的控制信息，这个字节的8个bit位表示不同的含义。
+    **/  
     *input++ = 'H'; *input++ = color;
     *input++ = 'e'; *input++ = color;
     *input++ = 'l'; *input++ = color;
@@ -23,6 +30,22 @@ int kern_entry()
     *input++ = 'e'; *input++ = color;
     *input++ = 'l'; *input++ = color;
     *input++ = '!'; *input++ = color;
+    // 输出 github username：stardustman
+    *input++ = 's'; *input++ = color;
+    *input++ = 't'; *input++ = color;
+    *input++ = 'a'; *input++ = color;
+    *input++ = 'r'; *input++ = color;
+    *input++ = 'd'; *input++ = color;
+    *input++ = 'u'; *input++ = color;
+    *input++ = 's'; *input++ = color;
+    *input++ = 't'; *input++ = color;
+    *input++ = 'm'; *input++ = color;
+    *input++ = 'a'; *input++ = color;
+    *input++ = 'n'; *input++ = color;
+    *input++ = '!'; *input++ = color;
+    *input++ = '!'; *input++ = color;
+    
+
 
     return 0;
 }
